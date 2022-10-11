2e94c0de1112a636da7e86183c044f54a1ad0414
atarikaltunn <atarikaltunn@gmail.com>
2022-10-11 15:37:35 +0300

    First commit of the repo, first three examples are added

---
+++ hello.js
@@ -0,0 +1,24 @@
+const { graphql, buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+    type Query{
+        hello: String
+    }
+`);
+
+// The rootValue provides a resolver function for each API endpoint
+var rootValue = {
+  hello: () => {
+    return "Hello World!";
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql({
+  schema,
+  source: '{ hello }',
+  rootValue,
+}).then((response) => {
+  console.log(response);
+});
